9fa2109c9fc9383373f98e374ad264ee0fdcae09
Abdul-Rahman Ibrahim <89408640+Abdul-Rahman-Ibrahim@users.noreply.github.com>
2022-09-28 01:00:14 +0300

    Update merge sort.py

--- sorting algorithms/merge sort.py
+++ sorting algorithms/merge sort.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python3
-
-class Order:
-    def __init__(self, id: int, selection_time: int, shipping_time: int):
-        self.id: int = id
-        self.selection_time: int = selection_time
-        self.shipping_time:  int = shipping_time
-
-
 def merge_sort(orders):
     '''
     TODO: Implement your sorting function and add the parameters you need.
@@ -38,32 +30,3 @@ def merge(arr,left,right):
         arr[k] = right[j]
         j += 1
         k += 1
-
-if __name__ == '__main__':
-    '''
-    Retrieves and splits the input
-    '''
-    data = input()
-    data = data.split('; ')
-    orders = []
-    for d in data:
-        id, selection_t, shipping_t = d.split(', ', 2)
-        order: Order = Order(int(id), int(selection_t), int(shipping_t))
-        '''
-        TODO: Append the `order` object to your structure.
-        '''
-        orders.append(order)
-    
-    '''
-    TODO: Call your sorting function
-    '''
-    merge_sort(orders)
-
-    '''
-    TODO: Print in stdout the id's of the sorted list (order.id) in one line separated by spaces.
-    Terminate with a new line.
-            i.e: 1 2 3 4 5 6\n
-    '''
-    for order in orders:
-        print(order.id, end=" ")
-    print("\n")
