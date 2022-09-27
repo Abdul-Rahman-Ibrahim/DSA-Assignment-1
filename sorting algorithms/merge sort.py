7fc37ce4d5f2d77a0979cca87a34605f032d5fef
Abdul-Rahman Ibrahim <89408640+Abdul-Rahman-Ibrahim@users.noreply.github.com>
2022-09-28 01:26:29 +0300

    Update merge sort.py

--- sorting algorithms/merge sort.py
+++ sorting algorithms/merge sort.py
@@ -1,32 +1,37 @@
-#!/usr/bin/env python3
-def merge_sort(orders):
-    '''
-    TODO: Implement your sorting function and add the parameters you need.
-    '''
-    if len(orders)>1:
-        mid = len(orders)//2
-        left = orders[:mid]
-        right = orders[mid:]
+#merge sort
+def merge_sort(array):
+    if len(array)>1:
+        mid = len(array)//2
+        left = array[:mid]
+        right = array[mid:]
+        
         merge_sort(left)
         merge_sort(right)
-        merge(orders,left,right)
+        merge(array,left,right)
 
-def merge(arr,left,right):
+def merge(array,left,right):
     i,j,k = 0,0,0
     while i<len(left) and j<len(right):
-        if left[i].selection_time+left[i].shipping_time<=right[j].selection_time+right[j].shipping_time:
-            arr[k] = left[i]
+        if left[i]<=right[j]:
+            array[k] = left[i]
             i += 1
         else:
-            arr[k] = right[j]
+            array[k] = right[j]
             j += 1
         k += 1
 
     while i<len(left):
-        arr[k] = left[i]
+        array[k] = left[i]
         i += 1
         k += 1
+
     while j<len(right):
-        arr[k] = right[j]
+        array[k] = right[j]
         j += 1
         k += 1
+##test
+array = [9,2,3,9,4,1,0,3,78,9,7]
+x = array.copy()
+merge_sort(array)
+x.sort()
+print(array==x)
